69098ab6a304c694c20ff17c22c13abd63f388d6
Nicholas Bekeris <nicholas@b3kndev.com>
2017-09-11 21:36:10 -0500

    Reset using angular-starter from @AngularClass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,66 @@
-import { TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+  inject,
+  async,
+  TestBed,
+  ComponentFixture
+} from '@angular/core/testing';
 
+/**
+ * Load the implementations that should be tested
+ */
 import { AppComponent } from './app.component';
+import { AppState } from './app.service';
 
-describe('App', () => {
+describe(`App`, () => {
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  /**
+   * async beforeEach
+   */
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [AppState]
+    })
+    /**
+     * Compile template and css
+     */
+    .compileComponents();
+  }));
+
+  /**
+   * Synchronous beforeEach
+   */
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    fixture = TestBed.createComponent(AppComponent);
+    comp    = fixture.componentInstance;
+
+    /**
+     * Trigger initial data binding
+     */
+    fixture.detectChanges();
   });
 
-  it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+  it(`should be readly initialized`, () => {
+    expect(fixture).toBeDefined();
+    expect(comp).toBeDefined();
   });
+
+  it(`should be @AngularClass`, () => {
+    expect(comp.url).toEqual('https://twitter.com/AngularClass');
+    expect(comp.angularclassLogo).toEqual('assets/img/angularclass-avatar.png');
+    expect(comp.name).toEqual('Angular 2 Webpack Starter');
+  });
+
+  it('should log ngOnInit', () => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled();
+
+    comp.ngOnInit();
+    expect(console.log).toHaveBeenCalled();
+  });
+
 });
